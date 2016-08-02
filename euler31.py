b01c98857be714d16fd0e9b9dd0e8ff1d22705c6
Mick Larson <mick@tunapanda.org>
2016-08-02 18:35:13 +0300

    more euler 31

--- euler31.py
+++ euler31.py
@@ -1,31 +1,15 @@
-a = 1
-b = 2
-c = 5
-d = 10
-e = 20
-f = 50
-g = 100
-h = 200
+def get_coin_combos(amount):
+    combos = []
+    for pennies in range(0,(amount+1)):
+        count = pennies*1
+        for twopence in range(0,(amount+1)/2):
+            count = pennies*1 + twopence*2
+            for fivepence in range(0,(amount+1)/5):
+                count = pennies*1 + twopence*2 + fivepence*5
+                for tenpence in range(0,(amount+1)/10):
+                    count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10
+                    if count == amount:
+                        combos.append([pennies,twopence,fivepence,tenpence,count])
+    return combos
 
-def get_permutations(amount):
-    permutations =[]
-    for pound in range(0,amount+1,100):
-        running_tally = pound
-        for fiftypence in range(0,amount+1-running_tally,50):
-            running_tally += fiftypence
-            for twentypence in range(0,amount+1-running_tally,20):
-                running_tally += twentypence
-                for tenpence in range(0,amount+1-running_tally,10):
-                    running_tally += tenpence
-                    for fivepence in range(0,amount+1-running_tally,5):
-                        running_tally+= fivepence
-                        for twopence in range(0,amount+1-running_tally,2):
-                            running_tally+= twopence
-                            for pennies in range(0,amount+1-running_tally,1):
-                                running_tally += pennies
-                                if running_tally == amount:
-                                    permutations.append([pennies,twopence,fivepence,tenpence,twentypence,fiftypence,pound])
-                                    running_tally-=pennies
-    return permutations
-
-print get_permutations(10)
+print get_coin_combos(10)
