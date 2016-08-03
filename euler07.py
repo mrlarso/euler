eb9121d1e20be3e34d5bd29f61ed94af0ae73a2a
Mick Larson <mick@tunapanda.org>
2016-08-03 10:13:56 +0300

    some more

--- euler07.py
+++ euler07.py
@@ -1,19 +1,18 @@
-def check_if_prime(x):
-    y = 2
-    while y < x:
-        if x % y == 0:
-            prime = False
-            break
-        else:
-            prime = True
-            y += 1
-    return prime
+import math
+def is_prime(number):
+	if number%2 == 0:
+		return False
+	for divisor in range(3,int(math.sqrt(number))+1,2):
+		if number%divisor == 0:
+			return False
+	return True
 
-primes_list = [2,3]
-x = 4
-while len(primes_list) < 10001:
-    if check_if_prime(x) == True:
-        primes_list.append(x)
-    x += 1
+n = 2
+test = 3
+while n < 1001:
+    if is_prime(test):
+        n +=1
+    test += 2
 
-print primes_list[-1]
+print str(n)
+print str(test)
--- euler38.py
+++ euler38.py
@@ -5,3 +5,20 @@ def is_pandigital(number):
         mylist.append(int(i))
     mylist.sort()
     return mylist == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def largest_pandigital():
+    number = 1
+    while len(str(number*1)+str(number*2)) <=9:
+        chain = ""
+        n = 1
+        while len(str(chain)) < 9:
+            chain += str(number*n)
+            n += 1
+        if len(chain) == 9 and is_pandigital(int(chain)):
+            print chain
+            print number
+            print str(n)
+            print "\n"
+        number += 1
+
+largest_pandigital()
