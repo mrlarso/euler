c89e0fe4a8b084e9e1932392d1e3d0e3053c4f20
Larson <larson@Larsons-MacBook-Air.local>
2016-08-03 22:24:31 +0300

    stupid primes (37)

---
+++ euler37.py
@@ -0,0 +1,39 @@
+import math
+
+#trying to get is_prime to evaluate to true if 2
+def is_prime(number):
+    if number == 2:
+        return True
+	if number%2 == 0 or number == 1:
+		return False
+	for divisor in range(3,int(math.sqrt(number))+1,2):
+		if number%divisor == 0:
+			return False
+	return True
+
+def get_truncations(number):
+    numberstring = str(number)
+    truncations = []
+    for index in range(1,len(numberstring)):
+        truncations.append(int(numberstring[:index]))
+        truncations.append(int(numberstring[-index:]))
+    return truncations
+
+def all_truncations_prime(truncations):
+    for i in truncations:
+        if not is_prime(i):
+            return False
+    return True
+"""
+print all_truncations_prime(get_truncations(3797))
+
+truncatable_primes = []
+x = 11
+while len(truncatable_primes) < 11:
+    if is_prime(x) and all_truncations_prime(get_truncations(x)):
+        truncatable_primes.append(x)
+        print x
+    x += 2
+
+print truncatable_primes
+"""
--- euler49.py
+++ euler49.py
@@ -20,12 +20,25 @@ def get_prime_perms(number):
     for x in range(number, 10000,2):
         if (sorted(get_intlist(number)) == sorted(get_intlist(x))) and is_prime(x):
             perms.append(x)
-    return perms
+    return sorted(perms)
 
-print get_prime_perms(1487)
-"""
-for i in range(1001,10000):
+def get_differences(numberlist):
+    differences = {}
+    for index in range(0,len(numberlist)):
+        for otherindex in range(index+1,len(numberlist)):
+            difference = numberlist[otherindex] - numberlist[index]
+            if difference in differences:
+                differences[difference] += 1
+            else:
+                differences[difference] = 1
+    return differences
+
+for i in range(1001,10000,2):
     if is_prime(i) and len(get_prime_perms(i)) > 2:
-        print i
-        print get_prime_perms(i)
-"""
+        differences = get_differences(get_prime_perms(i))
+        for item in differences:
+            if differences[item] >= 2:
+                print i
+                print get_prime_perms(i)
+                print differences
+                raw_input()
