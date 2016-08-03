6c2cbeef1d0f3eebc41fa6543cd1dace3274c6c2
Larson <larson@Larsons-MacBook-Air.local>
2016-08-03 20:15:26 +0300

    31 finished

--- euler31.py
+++ euler31.py
@@ -8,14 +8,16 @@ def get_coin_combos(amount):
                 count = pennies*1 + twopence*2 + fivepence*5
                 for tenpence in range(0,(amount-count)/10+1):
                     count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10
-                    for fiftypence in range(0,(amount-count)/50+1):
-                        count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10 +fiftypence*50
-                        for pound in range(0,(amount-count)/100+1):
-                            count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10 +fiftypence*50 + pound*100
-                            for twopound in range(0,(amount-count)/200+1):
-                                count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10 +fiftypence*50 + pound*100 + twopound*200
-                                if count == amount:
-                                    combos.append([pennies,twopence,fivepence,tenpence,fiftypence,pound,twopound,count])
+                    for twentypence in range(0,(amount-count)/20+1):
+                        count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10 + twentypence*20
+                        for fiftypence in range(0,(amount-count)/50+1):
+                            count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10 +fiftypence*50 + twentypence*20
+                            for pound in range(0,(amount-count)/100+1):
+                                count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10 +fiftypence*50 + pound*100 + twentypence*20
+                                for twopound in range(0,(amount-count)/200+1):
+                                    count = pennies*1 + twopence*2 + fivepence*5 + tenpence*10 +fiftypence*50 + pound*100 + twopound*200 + twentypence*20
+                                    if count == amount:
+                                        combos.append([pennies,twopence,fivepence,tenpence,fiftypence,pound,twopound,count])
     return len(combos)
 
 print get_coin_combos(200)
