1d1a08c03556daf6ef76e5819f57005ddd27bf10
Mick Larson <mick@tunapanda.org>
2016-08-16 08:48:01 +0300

    more on 23

--- euler23.py
+++ euler23.py
@@ -6,10 +6,27 @@ def get_divisors(number):
     return divisors
 
 def is_abundant(number):
-    elif sum(get_divisors(number)) > number:
+    if sum(get_divisors(number)) > number:
         return True
     return False
 
 def two_abundant(number):
     for i in range(1,(number/2)+1):
-        if is_abundant(i) and is_abundant(number-i)
+        if is_abundant(i):
+            if is_abundant(number-i):
+                return True
+    return False
+
+numberslist = range(1,28123)
+abundant_numbers_list = []
+for i in range (1,28123/2+1):
+    if i%1000 == 0:
+        print str(i)
+    if is_abundant(i):
+        for x in abundant_numbers_list:
+            if x+ i in numberslist:
+                numberslist.remove(x+i)
+        abundant_numbers_list.append(i)
+
+
+print two_abundant_list
